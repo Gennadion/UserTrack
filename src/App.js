697d2e43ff1cbfa191790c50437cce4ec577c427
Gennadion <ghammurti@gmail.com>
2022-08-16 13:38:23 +0300

    Input form to add users

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Card from './components/Card';
 import './App.css';
+import NewUser from './components/NewUser';
 
 function App() {
   return (
     <div className="App">
-      
+      <Card>
+        <NewUser />
+      </Card>
     </div>
   );
 }
