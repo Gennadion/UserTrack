896bfa1e2667c97684878246c2f26eb1999cd6b3
Gennadion <ghammurti@gmail.com>
2022-08-28 15:46:33 +0300

    Moved Modal and Backdrop up in DOM via Portals

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,11 +1,15 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import { Card } from "../Users/AddUser";
 import { Button } from "../Users/AddUser";
 
-const ErrorModal = (props) => {
+const Backdrop = (props) => {
+  return <Card><div className="backdrop" onClick={props.onConfirm}></div></Card>;
+};
+
+const ModalOverlay = (props) => {
   return (
     <Card>
-      <div className="backdrop" onClick={props.onConfirm}></div>
       <div className="modal">
         <header className="header">
           <h2>{props.title}</h2>
@@ -17,6 +21,25 @@ const ErrorModal = (props) => {
           <Button onClick={props.onConfirm}>Okay</Button>
         </footer>
       </div>
+    </Card>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <Card>
+      {ReactDOM.createPortal(
+        <Backdrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("modal-root")
+      )}
     </Card>
   );
 };
